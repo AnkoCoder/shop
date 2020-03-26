c4429133cfd91292444065913f959d5775d18b93
Anna <goodarzi.anna@gmail.com>
2020-03-26 14:12:40 +0600

    Add classes for 4 tables. Modified html file for new database

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, escape, request
 import json
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    products = relationship('Product', backref = 'category')
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, ForeignKey=('category.id'))
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    cost = db.Column(db.Integer, nullable=False)
+    items = relationship('OrderItem', backref = 'product')
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    telegram_id = db.Column(db.String(80), unique=True, nullable=False)
+    items = relationship('OrderItem', backref = 'order')
+
+class OrderItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, ForeignKey=('order.id'))
+    product_id = db.Column(db.Integer, ForeignKey=('product.id'))
+    quantity = db.Column(db.Integer, nullable=False)
 
 @app.route('/')
-def products_list():
-    with open('db.json') as f:
-        products = json.load(f)
-    return render_template('products.html', catalog=products)
+def catalog():
+    catalog = Category.query.all()
+    return render_template('products.html', catalog=catalog)
