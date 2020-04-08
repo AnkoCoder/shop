61fb20fbef91b26df46421acc4ac43c14199b336
Anna <goodarzi.anna@gmail.com>
2020-04-08 23:26:41 +0600

    Add conversation bot with buttons

---
+++ inline_key_bot_and_convers.py
@@ -0,0 +1,68 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+updater = Updater(token='1115972626:AAH2Tnx3N3NRa0knkZJDedOS1Ny21RsbTxA', use_context=True)
+dispatcher = updater.dispatcher
+
+
+GENDER, AGE = range(2)
+
+
+def start(update, context):
+    keyboard = [
+        [InlineKeyboardButton('Man', callback_data='man'),
+         InlineKeyboardButton('Woman', callback_data='woman')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Choose your gender",
+        reply_markup=reply_markup
+    )
+    return GENDER
+
+def gender(update, context):
+    context.user_data['gender'] = update.callback_query.data
+    keyboard = [
+        [InlineKeyboardButton('10-20', callback_data='10-20'),
+        InlineKeyboardButton('20-32', callback_data='20-32'),
+        InlineKeyboardButton('32-45', callback_data='32-45')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Choose your age",
+        reply_markup=reply_markup
+    )
+    return AGE
+
+def age(update, context):
+    context.user_data['age'] = update.callback_query.data
+    age = context.user_data['age']
+    gender = context.user_data['gender']
+    message = 'Your gender is {} and age is in the range of {}'.format(gender, age)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=message
+    )
+
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+    states={
+        GENDER: [CallbackQueryHandler(gender)],
+        AGE: [CallbackQueryHandler(age)]
+
+    },
+    fallbacks=[]
+)
+
+
+dispatcher.add_handler(conv_handler)
+
+
+updater.start_polling()
